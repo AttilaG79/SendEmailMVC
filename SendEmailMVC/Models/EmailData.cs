89c8e467ca6e46c30ece5f0547d61f3a6414cad1
Attila <Attila@LAPTOP-2UHVGMJE>
2022-11-27 23:24:29 +0000

    Add project files.

---
+++ SendEmailMVC/Models/EmailData.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendEmailMVC.Models
+{
+    public class EmailData
+    {
+        public string From { get; set; }
+       
+        public string? To { get; set; }
+       
+        public string? Subject { get; set; }
+        
+        public string? Body { get; set; }
+
+        public string Password { get; set; }
+
+        public EmailData()
+        {           
+            From = "youremailaddress";
+            Password = "password";                                  
+        }
+    }
+}
